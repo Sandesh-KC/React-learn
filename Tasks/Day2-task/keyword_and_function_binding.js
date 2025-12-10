cff208fdba5c87581f67b7c1cd5a5fa0291e7f3f
Sandesh-KC <kessysandesh111@gmail.com>
2025-12-10 22:30:52 +0545

    remaining day 5 tasks

--- Tasks/Day2-task/keyword_and_function_binding.js
+++ Tasks/Day2-task/keyword_and_function_binding.js
@@ -49,3 +49,67 @@ firstObj();
 secondObj();
 
 //Task 6: creating function in strict mode and using "this"
+function strictFun() {
+  "use strict";
+  const obj3 = {
+    name2: "Marcus",
+    detroit: function() {
+      console.log(`I am ${this.name2}`);
+    }
+  };
+  const thirdObj = obj3.detroit.bind(obj3);
+  thirdObj();
+}
+strictFun();
+
+//Task 7: 'this' does not work in arrow function
+const obj4 = {
+  name3: "connor",
+  secondDetroit: ()=> {
+    console.log(`It was meant to be ${this.name3}`);
+  }
+};
+obj4.secondDetroit();
+
+//Task 8: using 'this' in a nested object
+const obj5 = {
+  name4: "Kara",
+  details: {
+    species: "Android",
+    age: 30,
+    detKar: function() {
+      console.log(`She is ${this.species} and ${this.age} years old model.`);
+    }
+  },
+  showDetails: function() {
+    console.log(`This is ${this.name4}.`);
+  }
+};
+obj5.showDetails();
+obj5.details.detKar();
+
+//Task 9: using .call() to borrow method
+const person = {
+  name5: "Michael",
+  gta: function(item) {
+    console.log(item + " this is "+ this.name5);
+  }
+};
+person.gta("Hello");
+const person2 = {
+  name5: "Franklin"
+}
+person.gta.call(person2, "Hi");
+
+//Task 10: using .apply() to borrow method
+const person3 = {
+  name6: "Kratos",
+  gow: function(message, title) {
+    console.log(`${message}, ${this.name6} the ${title}.`);
+  }
+};
+person3.gow("Hello", "God");
+const person4 = {
+  name6: "Loki"
+}
+person3.gow.apply(person4, ["Hi", "Demigod"]);
